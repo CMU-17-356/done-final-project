d86db7a0b1c2266dcf5bb924bfd6a134d35f29d7
SylviaZhang211 <sylviazhang211@gmail.com>
2023-04-18 20:49:41 -0400

    added HistoryCard component, HistoryView view

---
+++ frontend/src/views/HistoryView.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react'
+import HisotryCard from '../components/HistoryCard'
+
+
+export default function History(){
+    return(
+        <h1>Calendar View</h1>
+    )
+    
+}
